6fa219bd2033a94c3713d7486b42cec8fb078e7e
Ehteshamali-889 <webdeveloper3628@gmail.com>
2025-05-05 21:31:01 +0500

    fix

--- src/CustomLoaderServiceProvider.php
+++ src/CustomLoaderServiceProvider.php
@@ -1,27 +1,22 @@
 <?php
 
-namespace Custom\Loader;
+namespace Ehteshamali\Loader;
 
 use Illuminate\Support\ServiceProvider;
 
 class CustomLoaderServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        // If needed, you could bind the model here
-    }
-
     public function boot()
     {
         $customModelPath = app_path('Custom.php');
-        
+
         if (file_exists($customModelPath)) {
             require_once $customModelPath;
         } else {
-            // Optional: Publish a stub if Custom.php doesn't exist
             $this->publishes([
                 __DIR__ . '/../Custom.php' => app_path('Custom.php'),
             ], 'custom-model');
         }
     }
 }
+
